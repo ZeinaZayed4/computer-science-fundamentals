43991fce4b8ce537cf984fabba55b99062cb1698
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-07-06 21:45:46 +0300

    Add example and notes for C# properties and encapsulation

---
+++ 2- OOP/04- Properties/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace oop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dollar dollar = new Dollar(1.4m);
+            dollar.SetAmount(0.4m); // set
+            Console.WriteLine(dollar.Amount); // get
+            Console.WriteLine(dollar.IsZero);
+            Console.WriteLine(dollar.ConversionFactor);
+            Console.ReadKey();
+        }
+    }
+
+    class Dollar
+    {
+        private decimal _amount;
+
+        // public decimal Amount { get; set; }
+        
+        public decimal Amount
+        {
+            get => this._amount;
+            private set => this._amount = ProcessValue(value);
+        }
+
+        public bool IsZero => _amount == 0;
+
+        public decimal ConversionFactor { get; } = 1.2m;
+
+        public void SetAmount(decimal value)
+        {
+            Amount = value;
+        }
+
+        public Dollar(decimal amount)
+        {
+            this._amount = ProcessValue(amount);
+        }
+
+        private decimal ProcessValue(decimal value) => (value <= 0) ? 0 : Math.Round(value, 2);
+    }
+}
---
+++ 2- OOP/04- Properties/Properties.md
@@ -0,0 +1,10 @@
+# Properties
+
+- They promote the encapsulation.
+- Public way to access field.
+
+## Syntax
+- `access_modifier data_type_of_the_field name_of_the_field`
+
+## Backing Field
+- The field that we will store and get values from.
