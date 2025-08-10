9ac294aac5f4468e2b78e96b3e39cdf65a0cf77b
ZeinaZayed4 <zeinazayed@std.mans.edu.eg>
2025-08-11 00:20:32 +0300

    OOP C#: finalizer

---
+++ 2- OOP/YouTube/09- Finalizer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Finalizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeSomeGarbage();
+            Console.WriteLine($"Memory used before collection {GC.GetTotalMemory(false):N0}");
+            GC.Collect(); // explicit cleaning
+            Console.WriteLine($"Memory used after collection {GC.GetTotalMemory(true):N0}");
+            Console.ReadKey();
+        }
+
+        static void MakeSomeGarbage()
+        {
+            Version v;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                v = new Version();
+            }
+        }
+    }
+}
